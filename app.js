93ebd67b4ff74b33b45488f0e42da7b3e1fd3a67
Mesh1313 <mesh13131@gmail.com>
2017-12-09 17:39:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var http = require('http').Server(app);
 var router = require('./server/router');
 var controllers = require('./server/controllers');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    server_ip_address   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 global.users = {};
 
